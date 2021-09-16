a8e323dc570fc50a939013e7b99ddf19d84cb0ee
José García <josgarci@student.42madrid.com>
2021-09-16 10:40:31 +0200

    ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,31 +6,14 @@
 /*   By: josgarci <josgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 17:30:17 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/14 12:16:03 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/09/16 10:30:29 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_salpha(int c)
+int	ft_isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 
-#include <stdio.h>
-int main(void)
-{
-	char c = 'X';
-	printf("%i\n", c);
-	return 0;
-}
-
-/*
-int	ft_salpha(int c)
-{
-	if (str[counter] < 'A' || (str[counter] > 'Z' && str[counter] < 'a')
-		|| str[counter] > 'z')
-		return (0);
-	return (1);
-}
-*/
 // NORM OK
 // SIN TEST
