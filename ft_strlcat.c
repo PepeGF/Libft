ca6141dcc3ceb549fbf1d620b2d42e2535609277
PepeGF <gran.pepe@gmail.com>
2021-09-22 19:19:38 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,19 +6,30 @@
 /*   By: josgarci <josgarci@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 16:42:39 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/22 18:53:59 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/09/22 19:19:01 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <string.h>
-#include "libtf.h"
+#include <bsd/string.h>//solo para linux
+// compilar con -lbsd
+#include "libft.h"
 
-size_t	strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	(void *)dst;
-	(void *)src;
+	(void )dst;
+	(void )src;
 	return (size);
 }
 
 int main ()
-{}
+{
+	char dst[] = "Aupa Atleti campeon";
+	char src[] = "wololololollo";
+	int size = 10;
+
+	printf("Orig: %li\n", strlcat(dst, src, size));
+	printf("dst -> %s\n", dst);
+	printf("src -> %s\n", src);
+	return (0);
+}
