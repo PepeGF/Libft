8b7289ab9eaee04fcaa43f36921d1d4e7182537e
PepeGF <gran.pepe@gmail.com>
2021-09-22 18:54:09 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,19 @@
 /*   By: josgarci <josgarci@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 16:42:39 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/22 16:42:48 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/09/22 18:53:59 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+#include "libtf.h"
 
+size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	(void *)dst;
+	(void *)src;
+	return (size);
+}
 
+int main ()
+{}
