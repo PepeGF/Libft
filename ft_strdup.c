fdd40365206a3a20ce4f8fd7be271ad621692a0e
José García <josgarci@student.42madrid.com>
2021-09-23 13:51:36 +0200

    ft_calloc + ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/23 13:08:57 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/23 13:31:16 by josgarci         ###   ########.fr       */
+/*   Created: 2021/09/23 13:32:46 by josgarci          #+#    #+#             */
+/*   Updated: 2021/09/23 13:50:44 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strdup(const char *s1)
 {
-	void *ptr;
+	char	*ptr;
+	size_t	lens1;
 
-	ptr = (void *)malloc(size * count);
-	if (ptr == '\0')
+	lens1 = ft_strlen(s1);
+	ptr = (char *)malloc(sizeof(char) * (lens1 + 1));
+	if (ptr == NULL)
 		return (0);
-	ft_bzero(ptr, size * count);
+	ft_strlcpy(ptr, s1, lens1 + 1);
 	return (ptr);
 }
