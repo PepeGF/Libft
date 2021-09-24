5ac22b535be273c4353f8331956b6493c7f144a0
José García <josgarci@student.42madrid.com>
2021-09-24 20:41:39 +0200

    ft_strtrim a medias

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,27 @@
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:26:07 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/24 17:46:22 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/09/24 20:38:34 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "llibft.h"
+#include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	 
+	size_t	first_diff;
+	size_t	last_diff;
+	char	*str;
+
+	if (!*s1)
+		return (0);
+	first_diff = 0;
+	last_diff = ft_strlen(s1);
+	while (s1 )
+	{
+		i = 0;
+		while (ft_strchr(s1, set[i]))
+			s1++;
+		first_diff++;
+	}
 }
