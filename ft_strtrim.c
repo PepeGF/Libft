9c371fc858b683d64f99b78092ff5db1a67c1503
José García <josgarci@student.42madrid.com>
2021-09-25 13:09:46 +0200

    that's what she said

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:26:07 by josgarci          #+#    #+#             */
-/*   Updated: 2021/09/24 23:52:54 by josgarci         ###   ########.fr       */
+/*   Updated: 2021/09/25 11:20:39 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	first_diff;
 	size_t	last_diff;
 
-	if (!*s1)
+	if (!s1 || !set)
 		return (0);
 	first_diff = 0;
 	last_diff = ft_strlen(s1) - 1;
 	while (s1[first_diff] && ft_strchr(set, s1[first_diff]))
 		first_diff++;
-	while (s1[last_diff] && ft_strchr(set, s1[last_diff]))
+	if (first_diff == last_diff + 1)
+		return (ft_strdup(""));
+	while (s1[last_diff] && ft_strrchr(set, s1[last_diff]))
 		last_diff--;
 	return (ft_substr(s1, first_diff, last_diff - first_diff + 1));
 }
